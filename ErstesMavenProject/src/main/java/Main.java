bfbe41d262768a1d9cc75d179b1c1d69a166e5cd
robert <robert@DESKTOP-V0OD2AI.fritz.box>
2017-12-17 12:45:43 +0100

    Demo commit.

---
+++ ErstesMavenProject/src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public Main() {
+		System.out.println("Ausgabe im Konstruktor.");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Start.");
+		Main main = new Main();
+		System.out.println("Ende.");
+
+	}
+
+}
